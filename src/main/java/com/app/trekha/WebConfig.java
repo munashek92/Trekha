bbb9a50659e7ce8ff38b0116c98355180db185ac
Munashe <munashek92@gmail.com>
2025-06-28 14:34:00 +0200

    28th commit

--- src/main/java/com/app/trekha/WebConfig.java
+++ src/main/java/com/app/trekha/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
 
 
 @Configuration
@@ -11,10 +12,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Value("${trekha.storage.location:uploads}")
     private String storageLocation;
-
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/" + storageLocation + "/**")
                 .addResourceLocations("file:" + storageLocation + "/");
     }
+    
 }
